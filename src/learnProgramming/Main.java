0684b251e90e87ee2aea1e10d5fca7b9b1f97990
poornima S <poornii87@gmail.com>
2019-08-18 08:56:54 +0100

    first commit

---
+++ src/learnProgramming/Main.java
@@ -0,0 +1,11 @@
+package learnProgramming;
+
+public class Main {
+    public static void main(String[] args) // Signature
+     {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+        System.out.println(args[3]);
+    }
+}
